84b2c5a1cc814b8193287b54ce45e2a59ec9a05c
Park Youngchan <rornf0322@naver.com>
2022-11-17 09:45:21 +0900

    11.17

---
+++ src/ch03.react_component/App_branching.js
@@ -0,0 +1,14 @@
+import {Component} from 'react';
+
+class App extends Component{
+  render(){
+    return(
+        <div>하하하 ~~hahah
+        </div>
+      );
+  }
+
+}
+
+// 클래스 이름과 동일해야 합니다.
+export default App;
